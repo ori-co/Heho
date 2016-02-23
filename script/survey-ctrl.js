f0005aa55ef4c6820d90275dfa98e1fc6704c64c
Oriane <oriane.cosson@eleves.ec-nantes.fr>
2016-02-23 12:54:35 +0100

    bouton next

--- script/survey-ctrl.js
+++ script/survey-ctrl.js
@@ -1,9 +1,9 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope) {
-    $scope.sample= [{id:0,identicon:'01',url:'samples/sample1.wav', etat:''},
-					{id:1,identicon:'02',url:'samples/sample1.wav', etat:''},
-					{id:2,identicon:'03',url:'samples/sample1.wav', etat:''},
+    $scope.sample= [{id:0,identicon:'01',url:'samples/sample1.wav', etat:'icon_active'},
+					{id:1,identicon:'02',url:'samples/sample1.wav', etat:'icon_disabled'},
+					{id:2,identicon:'03',url:'samples/sample1.wav', etat:'icon_disabled'},
 					{id:3,identicon:'04',url:'samples/sample1.wav', etat:'icon_disabled'},
 					{id:4,identicon:'05',url:'samples/sample1.wav', etat:'icon_disabled'},
 					{id:5,identicon:'06',url:'samples/sample1.wav', etat:'icon_disabled'},
@@ -13,7 +13,7 @@ app.controller('myCtrl', function($scope) {
 					{id:9,identicon:'10',url:'samples/sample1.wav', etat:'icon_disabled'}];
 					
 	$scope.param=[{nom:'caractéristique 1',description:'description correspndant à la caractéristique 1',valMin:'non mot clé 1', valMax:'très mot clé 1'}, 
-					{nom:'caractéristique 2',description:'description correspndant à la caractéristique 2',valMin:'non mot clé 1', valMax:'très mot clé 1'}, 
+					{nom:'caractéristique 2',description:'description correspndant à la caractéristique 2',valMin:'non mot clé 2', valMax:'très mot clé 2'}, 
 					{nom:'Naturel',description:'exemple',valMin:'pas naturel', valMax:'très naturel'}];
 					
 	$scope.current=$scope.sample[0];
@@ -22,5 +22,11 @@ app.controller('myCtrl', function($scope) {
 		$scope.current.etat = '';
 		$scope.current=$scope.sample[id];
 		$scope.current.etat = 'icon_active';
+	}
+	
+	$scope.next=function(){
+		var idCurr = $scope.current.id;
+		$scope.sample[idCurr]=$scope.current;
+		$scope.selection(idCurr+1);
 	}
 });
