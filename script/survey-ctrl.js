db0de529741a1d2021b49b6b669676a4ccf7229b
Henri Chain <henri.chain@eleves.ec-nantes.fr>
2016-02-23 14:59:11 +0100

    fix stupid mistake

--- script/survey-ctrl.js
+++ script/survey-ctrl.js
@@ -1,20 +1,20 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope) {
-    $scope.sample = [{id:0,identicon:'01',url:'samples/sample1.wav', etat:'icon_active'},
-                     {id:1,identicon:'02',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:2,identicon:'03',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:3,identicon:'04',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:4,identicon:'05',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:5,identicon:'06',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:6,identicon:'07',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:7,identicon:'08',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:8,identicon:'09',url:'samples/sample1.wav', etat:'icon_disabled'},
-                     {id:9,identicon:'10',url:'samples/sample1.wav', etat:'icon_disabled'}];
+    $scope.sample = [{id:0,identicon:'01', url:'samples/sample1.wav', etat:'icon_active'},
+                     {id:1,identicon:'02', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:2,identicon:'03', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:3,identicon:'04', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:4,identicon:'05', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:5,identicon:'06', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:6,identicon:'07', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:7,identicon:'08', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:8,identicon:'09', url:'samples/sample1.wav', etat:'icon_disabled'},
+                     {id:9,identicon:'10', url:'samples/sample1.wav', etat:'icon_disabled'}];
                     
-    $scope.param = [{nom:'caractéristique 1', description:'description correspndant à la caractéristique 1', valMin:'non mot clé 1', valMax:'très mot clé 1'}, 
-                    {nom:'caractéristique 2', description:'description correspndant à la caractéristique 2', valMin:'non mot clé 2', valMax:'très mot clé 2'}, 
-                    {nom:'Naturel', description:'exemple', valMin:'pas naturel', valMax:'très naturel'}];
+    $scope.param = [{nom:'caractéristique 1', description:'description correspndant à la caractéristique 1', valMin:'non mot clé 1', valMax:'très mot clé 1', notes: []}, 
+                    {nom:'caractéristique 2', description:'description correspndant à la caractéristique 2', valMin:'non mot clé 2', valMax:'très mot clé 2', notes: []}, 
+                    {nom:'Naturel', description:'exemple', valMin:'pas naturel', valMax:'très naturel', notes: []}];
                     
     $scope.current = $scope.sample[0];
 
@@ -30,9 +30,9 @@ app.controller('myCtrl', function($scope) {
         $scope.selection(idCurr + 1);
     }
     
-    $scope.note=function($index,$event){
+    $scope.note = function($index, $event){
         var idCurr = $scope.current.id;
-        $scope.param[$index].notes[idCurr]=$event.offsetX;
+        $scope.param[$index].notes[idCurr] = $event.offsetX;
         //alert("note de "+idCurr+" sur le critère "+$scope.param[$index].nom+" est de : "+$scope.param[$index].notes[idCurr]);
     }
 });
