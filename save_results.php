b71a04a0b0a1d11ea2815dee6076446d42feb2d2
ori-co <ori.co78@gmail.com>
2016-10-23 22:48:53 +0200

    initial commit on the new github repo

--- save_results.php
+++ save_results.php
@@ -1,6 +1,12 @@
 <?php
    $json = file_get_contents("php://input");
    
+   /*$postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    @$exp = $request->exp;
+    @$name = $request->name;
+    @$results = $request->results;*/
+
    //$json = $_POST['json'];
    //$arr = json_decode($json);
    //$file = fopen('results/' . $json->timestamp . '.json', 'w+');
--- script/survey-ctrl.js
+++ script/survey-ctrl.js
@@ -9,73 +9,51 @@ app.controller('myCtrl', function($scope,$timeout,$http) {
     
     $scope.player = document.getElementById("audioplayer");
     
-    $scope.button=document.getElementById("buttonNext");     
+    $scope.button=document.getElementById("buttonNext");   
+
+    $scope.exp = Math.random() <=0.5;
+    
+    $scope.startTime;
     
     $scope.init = function (exp){
+        $scope.count++;
         switch (exp){
-            case 1 :
+            case true:
             // Initialisation expérience 1
-                $scope.sample = [{id:0,identicon:'01', url:'samples/Crea Compositeur Brief 1_1.wav', etat:'icon_disabled'},
-                     {id:1,identicon:'02', url:'samples/Echantillon Max Msp_Brief Vegetal.wav', etat:'icon_disabled'},
-                     {id:2,identicon:'03', url:'samples/brief_vegetal_prop_1_classLvl_1_rep_1.wav', etat:'icon_disabled'},
-                     {id:3,identicon:'04', url:'samples/brief_vegetal_prop_1_classLvl_1_rep_2.wav', etat:'icon_disabled'},
-                     {id:4,identicon:'05', url:'samples/brief_vegetal_prop_1_classLvl_1_rep_3.wav', etat:'icon_disabled'},
-                     {id:5,identicon:'06', url:'samples/brief_vegetal_prop_1_classLvl_2_rep_1.wav', etat:'icon_disabled'},
-                     {id:6,identicon:'07', url:'samples/brief_vegetal_prop_1_classLvl_2_rep_2.wav', etat:'icon_disabled'},
-                     {id:7,identicon:'08', url:'samples/brief_vegetal_prop_1_classLvl_2_rep_3.wav', etat:'icon_disabled'}];
-                     //{id:8,identicon:'09', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:9,identicon:'10', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:10,identicon:'11', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:11,identicon:'12', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:12,identicon:'13', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:13,identicon:'14', url:'samples/sample1.wav', etat:'icon_disabled'}];
-                
-                $scope.sample = $scope.sample.sort(function() { return 0.5 - Math.random() });                
+                $scope.sample = [{id:0,identicon:'01', url:'samples/Crea_Compositeur_Brief_1_1.wav', etat:'icon_disabled'},
+                     {id:1,identicon:'02', url:'samples/Echantillon_Max_Msp_Brief_Vegetal.wav', etat:'icon_disabled'},
+                     {id:2,identicon:'03', url:'samples/brief_vegetal_prop_1_classLvl_2_rep_4.wav', etat:'icon_disabled'},
+                     {id:3,identicon:'04', url:'samples/brief_vegetal_prop_2_classLvl_1_rep_5.wav', etat:'icon_disabled'},
+                     {id:4,identicon:'05', url:'samples/brief_vegetal_prop_2_classLvl_2_rep_5.wav', etat:'icon_disabled'}];              
                     
-                $scope.param = [{nom:'Naturelle', description:'vent, ruisseau ...', notes: []}, 
-                                {nom:'Végétale', description:'bruits de feuilles ...', notes: []},
-                                {nom:'Organique', description:'pas humains, bruits d animaux ...', notes: []},  
+                $scope.param = [{nom:'Naturelle', description:'vent, ruisseau,bruits d animaux ...', notes: []}, 
+                                {nom:'Végétale', description:'bruits de feuilles ...', notes: []}, 
                                 {nom:'Bucolique', description:'propice à la balade et à la rêverie', notes: []},
-                                {nom:'Agréable', description:'', notes: []}];
+                                {nom:'Agréable', description:'écoute plaisante', notes: []},
+                                {nom:'Etrange', description:'écoute perturbante, déstabilisante', notes: []}];
                     
-                $scope.current =0;
-                $scope.selection($scope.current);
-                $scope.delay();
                 break;
                 
-            case 2:
+            case false:
             // Initialisation expérience 2
-                $scope.sample = [{id:0,identicon:'01', url:'samples/Crea Compositeur Brief 2_2.wav', etat:'icon_disabled'},
-                     {id:1,identicon:'02', url:'samples/Echantillon Max Msp_Brief Technologique.wav', etat:'icon_disabled'},
-                     {id:2,identicon:'03', url:'samples/brief_technologique_prop_2_classLvl_1_rep_1.wav', etat:'icon_disabled'},
+                $scope.sample = [{id:0,identicon:'01', url:'samples/Crea_Compositeur_Brief_2_2.wav', etat:'icon_disabled'},
+                     {id:1,identicon:'02', url:'samples/Echantillon_Max_Msp_Brief_Technologique.wav', etat:'icon_disabled'},
+                     {id:2,identicon:'03', url:'samples/brief_technologique_prop_1_classLvl_2_rep_3.wav', etat:'icon_disabled'},
                      {id:3,identicon:'04', url:'samples/brief_technologique_prop_2_classLvl_1_rep_2.wav', etat:'icon_disabled'},
-                     {id:4,identicon:'05', url:'samples/brief_technologique_prop_2_classLvl_1_rep_3.wav', etat:'icon_disabled'},
-                     {id:5,identicon:'06', url:'samples/brief_technologique_prop_2_classLvl_2_rep_1.wav', etat:'icon_disabled'},
-                     {id:6,identicon:'07', url:'samples/brief_technologique_prop_2_classLvl_2_rep_2.wav', etat:'icon_disabled'},
-                     {id:7,identicon:'08', url:'samples/brief_technologique_prop_2_classLvl_2_rep_3.wav', etat:'icon_disabled'}];
-                     //{id:8,identicon:'09', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:9,identicon:'10', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:10,identicon:'11', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:11,identicon:'12', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:12,identicon:'13', url:'samples/sample1.wav', etat:'icon_disabled'},
-                     //{id:13,identicon:'14', url:'samples/sample1.wav', etat:'icon_disabled'}];
-                
-                $scope.sample = $scope.sample.sort(function() { return 0.5 - Math.random() });
-                
+                     {id:4,identicon:'05', url:'samples/brief_technologique_prop_2_classLvl_2_rep_1.wav', etat:'icon_disabled'}];
+                              
                 $scope.param = [{nom:'Dynamique', description:'sonorités événementielles et ponctuelles', notes: []}, 
                                 {nom:'Entrainant', description:'loops rythmiques, pulsations', notes: []}, 
                                 {nom:'Technologique', description:'sons électroniques et séquences mélodiques synthétiques', notes: []},
-                                {nom:'Agréable', description:'', notes: []}];
-                $scope.current = 0;
-                $scope.selection($scope.current);
-                $scope.delay();
+                                {nom:'Agréable', description:'écoute plaisante', notes: []},
+                                {nom:'Etrange', description:'écoute perturbante, déstabilisante', notes: []}];
                 break;
-                
-            default:
-            // fin de l'expérience
-            $scope.button.className="button_disabled";
-            alert("Expérience terminée. Merci !");
         }
+        $scope.startTime = Date.now();
+        $scope.sample = $scope.sample.sort(function() { return 0.5 - Math.random() });
+        $scope.current =0;
+        $scope.selection($scope.current);
+        $scope.delay();
     }
     
     $scope.updateButtonStyle = function (){
@@ -98,9 +76,8 @@ app.controller('myCtrl', function($scope,$timeout,$http) {
         $timeout( function(){  
              $scope.delayOK = true;
              $scope.updateButtonStyle();
-        }, 60000);
+        }, 30000);
         // une minute par echantillon
-        //}, 30000);
     }
     
     $scope.allRes = false;
@@ -137,13 +114,21 @@ app.controller('myCtrl', function($scope,$timeout,$http) {
         } else {
             // Sauvegarde et passage à la deuxième expérience
             $scope.saveResults();
-            $scope.exp ++;
-            $scope.init($scope.exp);
+            if ($scope.count==1){
+                $scope.count++;
+                $scope.exp = !$scope.exp;
+                $scope.init($scope.exp);
+            } else {
+                //fin de l'expérience
+                $scope.button.className="button_disabled";
+                $scope.end=true;                
+            }
         }
     }
     
     $scope.note = function($index, $event){
-        var size = $event.srcElement.offsetWidth;
+        var target = $event.currentTarget || $event.srcElement;
+        var size = target.offsetWidth;
         $scope.param[$index].notes[$scope.current] = $event.offsetX/size*100;
         $scope.testAllRes();
         //alert("note de "+$scope.current+" sur le critère "+$scope.param[$index].nom+" est de : "+$scope.param[$index].notes[$scope.current]);
@@ -158,15 +143,19 @@ app.controller('myCtrl', function($scope,$timeout,$http) {
         for (i = 0; i < $scope.param.length; i++) { 
             temp.push({param:$scope.param[i].nom, notes:$scope.param[i].notes})
         }
+        var subject ={
+            name:$scope.name,
+            firstname:$scope.firstname,
+            age:$scope.age,
+            gender:$scope.gender
+        }
         var res = {
+            subjectInfo:subject,
             exp:$scope.exp,
+            start:$scope.startTime,
             sampleID:ids,
             notes:temp
             }
         $http.post("save_results.php",res);
-    }
-    
-    $scope.testfunc = function(scope, a) {
-        alert(a);
     }
 });
